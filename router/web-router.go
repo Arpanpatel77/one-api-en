3bda3c4ed1c00c32da06392dc9683f0cde4fcd35
Arpanpatel77 <118924322+Arpanpatel77@users.noreply.github.com>
2023-05-26 18:04:50 +0530

    Update web-router.go

--- router/web-router.go
+++ router/web-router.go
@@ -14,8 +14,8 @@ func SetWebRouter(router *gin.Engine, buildFS embed.FS, indexPage []byte) {
 	router.Use(gzip.Gzip(gzip.DefaultCompression))
 	router.Use(middleware.GlobalWebRateLimit())
 	router.Use(middleware.Cache())
-	router.Use(static.Serve("/", common.EmbedFolder(buildFS, "web/build")))
-	router.NoRoute(func(c *gin.Context) {
-		c.Data(http.StatusOK, "text/html; charset=utf-8", indexPage)
-	})
-}
+router.Use(static.Serve("/", common.EmbedFolder(buildFS, "web/build")))
+router.NoRoute(func(c *gin.Context) {
+	c.Header("Cache-Control", "no-cache, must-revalidate")
+	c.Data(http.StatusOK, "text/html; charset=utf-8", indexPage)
+})
